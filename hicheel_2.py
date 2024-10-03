095c55df41a601ea1e19a75f8c8c64df16ff32f7
enkhjin <129244493+Enzi001@users.noreply.github.com>
2024-10-03 08:27:16 +0800

    Add files via upload

---
+++ hicheel_2.py
@@ -0,0 +1,62 @@
+# phyton .py өргөтгөлтэй
+# мөр тус бүр шинээр эхэлдэг
+# a=3
+# print("") Хэвлэх
+# print("Hello world!")
+
+# str 
+# name = " bold "
+# age = 25
+
+# Дүрэм:
+# Хувьсагчийн нэр тоогоор эхлэхгүй.
+# Тусгай тэмдэгт (@, !, #, гэх мэт) агуулаагүй байх.
+# Зөвхөн үсэг, тоо, доогуур зураас (_) хэрэглэнэ.
+
+# height = 170
+# print(hei
+
+# int a бүхэл тоо
+# float f бутархай тоо
+# string data агуулсан 
+# boolean bool логик 
+
+# fruits = ["apple", "orange", "cherry"]
+
+# tuple 
+# coordinates = (10.5 , 5.8)
+
+# dict
+# students = {"name": "bold", 
+#             "age": 18 , 
+#             "isMarried": False}
+
+# name = input("Та нэрээ бичнэ үү")
+
+# age = int(input("Та насаа бичнэ үү"))
+
+# if 100> 90:
+#     print(name + " та насанд хүрсэн байна ")
+# else: 
+#     print(name + " насанд хүрээгүй байна")
+
+# if elif else for while
+
+# fruits = ["apple", "orange", "cherry"]
+# for fruit in fruits:
+#     print(fruit)
+
+# while
+# too = 1
+# while too <= 5:
+#     print("Давталт", too)
+#     too += 1
+
+
+niilber = 0
+i=1
+
+while i <= 20:
+    niilber += i
+    i += 1
+print(niilber)
